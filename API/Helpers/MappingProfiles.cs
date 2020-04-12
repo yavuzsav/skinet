62d272dc9944ebc7cab844ab21679fd65331e1a0
Yavuz SAV <yavuzsav07@gmail.com>
2020-04-13 00:36:37 +0300

    End of section 4

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(x => x.ProductBrand.Name))
+                .ForMember(dest => dest.ProductType, opt => opt.MapFrom(x => x.ProductType.Name))
+                .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
